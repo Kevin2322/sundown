8b1b9f529297a15c926b79addf8dec07a99e8f67
Kevin2322 <vankawalakevin153@gmail.com>
2024-01-04 18:00:37 +0530

    fourth commit

--- script.js
+++ script.js
@@ -4,3 +4,41 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+function page4Animation(){
+    var elemc = document.querySelector("#elem-container")
+var fixed = document.querySelector("#fixed-image")
+elemc.addEventListener("mouseenter",function(){
+    fixed.style.display = "block"
+})
+elemc.addEventListener("mouseleave",function(){
+    fixed.style.display = "none"
+})
+
+var elems = document.querySelectorAll(".elem")
+elems.forEach(function(e){
+
+    e.addEventListener("mouseenter",function(){
+        var image = e.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${image})`
+    
+})
+    
+    // console.log(image)
+})
+
+}
+function swiperAnimation(){
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 30,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+}
+
+swiperAnimation()
+page4Animation()
